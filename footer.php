790885b2ddd8d9c5b1bfd5eff44c68cf0672855e
REINIS BRUVELIS <reinisis.br@gmail.com>
2024-03-10 20:45:32 +0200

    parmainas

--- footer.php
+++ footer.php
@@ -1,34 +1,33 @@
-<footer style="width: 100%;">
-      <ul class="footer-links-main">
-        <li>Kontakti</li>
-        <li>Telefona nr.</li>
-        <li>e-pasts</li><br><br>
-        <li>@Reinis Brūvelis 2021</li>
-
-      </ul>
-      <ul class="footer-links-classes">
-        <li>
-          <br>
-        </li>
-        <li>28442903</li>
-        <li>reinisis.br@gmail.com</li>
+<div class="wrapper">
 
 
+<footer>
+  <ul class="footer-links-main">
+    <li><a href="#">Mājas</a></li>
+    <li><a href="cases.html">Uzdevumi</a></li>
 
-      </ul>
-      <div class="footer-sm">
-        <a href="https://www.youtube.com/channel/UCTnSei5Yh4zFl4h_fId-0PA">
-          <img src= "/wordpress/wp-content/uploads/2021/05/yt.png" alt="youtube logo" class="youtube">
-        </a>
-        <a href="https://lv-lv.facebook.com/people/Reinis-Br%C5%ABvelis/100016191489568/">
-          <img src="/wordpress/wp-content/uploads/2021/05/fb.png" alt="facebook logo">
-        </a>
-      </div>
-
-
-    </footer>
-
+    <li><a href="#">Par mums</a></li>
+    <li><a href="#">Contacts</a></li>
+  </ul>
+  <ul class="footer-links-classes">
+    <li>
+      <p>Latest cases</p>
+    </li>
+    <li><a href="cases.html">Daudz uzdevumu</a></li>
+    <li><a href="#">Par mums</a></li>
+    <li><a href="#">kontakti</a></li>
+  </ul>
+  <div class="footer-sm">
+    <a href="#">
+      <img src="yt.png" alt="youtube logo">
+    </a>
+    <a href="#">
+      <img src="fb.png" alt="facebook logo">
+    </a>
   </div>
+
+</footer>
+</div>
 </body>
 
 </html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'header.php';
+ ?>
+  <main>
+    <section class="index-banner">
+      <div class="vertical-center">
+        <h2>Finanšu <br> pratība</h2>
+        <h1>Interaktīva apmācības platforma.</h1>
+      </div>
+    </section>
+    <div class="wrapper">
+      <section class="index-links">
+        <a href="cases.html">
+          <div class="index-boxlink-square">
+            <h3>Uzdevumi</h3>
+          </div>
+        </a>
+        <a href="#">
+          <div class="index-boxlink-rectangle">
+            <h3>Jaunumi</h3>
+          </div>
+        </a>
+        <a href="#">
+          <div class="index-boxlink-square">
+            <h3>Par mums</h3>
+          </div>
+        </a>
+        <a href="#">
+          <div class="index-boxlink-square">
+            <h3>Kontakti</h3>
+          </div>
+        </a>
+      </section>
+    </div>
+  </main>
+
+  <?php
+include_once 'footer.php';
+ ?>
