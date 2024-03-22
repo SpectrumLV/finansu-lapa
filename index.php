78a712ff8825ce572857ac42d842232f904362d4
REINIS BRUVELIS <reinisis.br@gmail.com>
2024-03-22 18:57:47 +0200

    izveidots landing page

--- index.php
+++ index.php
@@ -3,30 +3,97 @@ include_once 'navbar.php';
  ?>
   <main>
     <section class="index-banner">
-      <div class="vertical-center">
-        <h2>Mūsu finanšu <br> pratība</h2>
-        <h1>Interaktīva apmācības platforma.</h1>
-      </div>
-    </section>
-    <div class="wrapper">
-      <section class="index-links">
-        <a href="cases.php">
-          <div class="index-boxlink-square">
-            <h3>Jaunumi</h3>
+            <div class="row">
+            <div class="col-md-4"style="padding: 0px 0px" >
+              <div class="blue-background">
+                <h2>Interaktīva finanšu pratības platforma</h2>
+                <h1>Atklāj finanšu pasauli!</h1>
+                <div class="btn" style="padding-right: 70px">
+                <button style="font-size: 1.4rem; width:100%; border-radius:20px; height: 70px">UZDEVUMI</button>
+                </div>
+                
+              </div>
+            </div>
+            <div class="col-md-8 landing-image">
+              <img src="landing-page.png" alt="Banner Image" class="img-fluid">
+            </div>
           </div>
-        </a>
-        <a href="cases.php">
-          <div class="index-boxlink-rectangle">
-            <h3>Uzdevumi</h3>
-          </div>
-        </a>
-        <a href="#">
-          <div class="index-boxlink-square">
-            <h3>Kontakti</h3>
-          </div>
-        </a>
+        </div>
+
       </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    <section class="case-vid">
+      
+     
+    <h1 style= "text-align: center; padding: 90px; font-size: 5rem; text-transform: none">Info par platformu</h1>
+      
+</section>
+
+
+    
+  
+
+  <div class="row" style="text-align: center; margin: 0 50px">
+
+
+    <div class="col-md-4 cards">
+      <div class="card">
+        <div class="card-body">
+          <h1 class="card-title">Mācies</h1>
+          <h3 class="card-text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney</h3>
+          <a class="btn btn-outline-dark btn-lg">Uz sadaļu</a>
+        </div>
+      </div>
     </div>
+
+
+    <div class="col-md-4 cards">
+      <div class="card">
+        <div class="card-body">
+          <h1 class="card-title">Praktizējies</h1>
+          <h3 class="card-text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney</h3>
+          <a class="btn btn-outline-dark btn-lg">Uz sadaļu</a>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-md-4 cards">
+      <div class="card">
+        <div class="card-body">
+          <h1 class="card-title">Izglītojies</h1>
+          <h3 class="card-text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney</h3>
+          <a class="btn btn-outline-dark btn-lg">Uz sadaļu</a>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+  </div>
+
+
+
+
+
+   
+
+
+
   </main>
 
   <?php
