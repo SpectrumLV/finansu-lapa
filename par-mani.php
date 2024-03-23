e35647eb559a008472d091667b2febbe9e9ca63f
REINIS BRUVELIS <reinisis.br@gmail.com>
2024-03-23 11:14:32 +0200

    izveidota par mums lapa

--- par-mani.php
+++ par-mani.php
@@ -1,17 +1,80 @@
 <?php include_once('navbar.php'); ?>
   <main>
-    <div class="wrapper par-mani">
+  <section class="case-vid" style="padding: 0 0;">
+       
+     
 
-      <h1>Par mums</h1>
-      <p>Mēs esam interaktīvā apmācību platforma, kas jauniešiem sniedz iespēju uzzināt vairāk par finansēm. 
-        <br> Mājaslapā ir vairāk kā 10 tēmas par savu finanšu pārvaldību <br><br>
-        Platformā sastāv no rakstiem. Katrā rakstā ir apskatīta viena tēma. Šī tēma ir pasniegta rakstiskā vai video materiāla veidā. Katras nodarbības beigās ir interaktīvs testiņš, kas pārbaudīs Tavas zināšanas</p>
+      <h1 style= "text-align: center; padding-bottom: 50px; font-size: 2rem; text-transform: none">Info par platformu</h1>
+        <div class="row" style= "text-align: left; margin-bottom: 50px; padding: 0 100px">
+      
+        
+          <div class="col-md-6" style= "padding: 0px 100px">
+            <h1 style= "text-align: left;font-size: 3rem; text-transform: none">Spēks</h1>
+            <br>
+            <h3 style= "font-size: 1.2rem">Inflācija apzīmē cenu pieauguma procesu. 
+Inflāciju mēra kā cenu vidējā līmeņa izmaiņu procentuālā izteiksmē
+noteiktā laika posmā - mēnesī, ceturksnī, gadā u.c.</h3>
+          </div>
+
+          <div class="col-md-6" style= "padding:0 100px">
+            <h1 style= "text-align: left;font-size: 3rem; text-transform: none">Degsme</h1>
+            <br>
+            <h3 style= "font-size: 1.2rem">Inflācija apzīmē cenu pieauguma procesu. 
+Inflāciju mēra kā cenu vidējā līmeņa izmaiņu procentuālā izteiksmē
+noteiktā laika posmā - mēnesī, ceturksnī, gadā u.c.</h3>
+          </div>
+      
+        </div>
 
 
-        <h2>Piesakies uz konsultāciju <a href="anketa.php">ŠEIT</a></h2>
-    </div>
+              <div class="row" style= "text-align: left; padding: 0 100px">
+            
+              
+                <div class="col-md-6"  style= "padding: 0 100px">
+                  <h1 style= "text-align: left;font-size: 3rem; text-transform: none">Neatkarība</h1>
+                  <br>
+                  <h3 style= "font-size: 1.2rem">Inflācija apzīmē cenu pieauguma procesu. 
+      Inflāciju mēra kā cenu vidējā līmeņa izmaiņu procentuālā izteiksmē
+      noteiktā laika posmā - mēnesī, ceturksnī, gadā u.c.</h3>
+                </div>
+
+                <div class="col-md-6"  style= "padding: 0 100px">
+                  <h1 style= "text-align: left;font-size: 3rem; text-transform: none">Drosme</h1>
+                  <br>
+                  <h3 style= "font-size: 1.2rem">Inflācija apzīmē cenu pieauguma procesu. 
+      Inflāciju mēra kā cenu vidējā līmeņa izmaiņu procentuālā izteiksmē
+      noteiktā laika posmā - mēnesī, ceturksnī, gadā u.c.</h3>
+                </div>
+            
+              </div>
+
+      
+</section>
 
 
+      <section class="index-banner">
+           <div class="row" style="margin: 70px 120px">
+
+           <div class="col-md-8 landing-image">
+              <img src="TEAM.jpg" alt="Banner Image" class="img-fluid">
+            </div>
+
+            <div class="col-md-4"style="padding: 0px 0px" >
+                <h4 style="font-size: 2.5rem; padding: 50px 0;color: #003398; font-weight: 700">Komandas spēks nav tikai kopējās pūles summa,
+              bet gan savstarpējās sadarbības kvalitāte un gatavība vienoties par kopējiem mērķiem.</h4>
+
+            </div>
+
+
+       
+        </div>
+
+      </section>
+
+
+
+
+          </section>  
 
 
 
